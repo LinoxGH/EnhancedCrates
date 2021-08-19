4805f00ff43e5973f75d331a40e3abcf1854216b
LinoxGH <denizcanaksu121@gmail.com>
2021-08-19 21:50:57 +0300

    Added some fail-safe checks to type command.

--- src/main/java/me/linoxgh/crates/Commands/TypeCommand.java
+++ src/main/java/me/linoxgh/crates/Commands/TypeCommand.java
@@ -5,6 +5,7 @@ import me.linoxgh.crates.Data.CrateType;
 import me.linoxgh.crates.Data.Key;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class TypeCommand extends Command {
@@ -28,11 +29,29 @@ public class TypeCommand extends Command {
 
         if (args.length != 3) return false;
         if (args[1].equals("add")) {
-            crates.addCrateType(args[2], new CrateType(new Key(p.getInventory().getItemInMainHand())));
+            ItemStack heldItem = p.getInventory().getItemInMainHand();
+            if (heldItem.getAmount() == 0 || heldItem.getType().isEmpty() || heldItem.getType().isAir()) {
+                p.sendMessage("§4You must hold the key item in your hand.");
+                return true;
+            }
+
+            CrateType crate = crates.getCrateType(args[2]);
+            if (crate != null) {
+                p.sendMessage("§4A crate type with this name already exists.");
+                return true;
+            }
+
+            crates.addCrateType(args[2], new CrateType(new Key(heldItem)));
             p.sendMessage("§aSuccessfully added a new crate type.");
             return true;
 
         } else if (args[1].equals("delete")) {
+            CrateType crate = crates.getCrateType(args[2]);
+            if (crate == null) {
+                p.sendMessage("§4Could not find the crate type.");
+                return true;
+            }
+
             crates.removeCrateType(args[2]);
             p.sendMessage("§aSuccessfully deleted this crate type.");
             return true;
